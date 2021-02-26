76e4ba545390ff56979a2ef6d9d419b44fb1f029
Arthurdw <mail@arthurdw.com>
2021-02-26 19:30:41 +0100

    🌟 Added logging.

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,3 +1,6 @@
+extern crate pretty_env_logger;
+#[macro_use] extern crate log;
+
 mod utils;
 
 use std::env;
@@ -14,33 +17,36 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, _ctx: Context, msg: Message) {
-        println!("Received message: {}", msg.content);
+        info!("Received message: {}", msg.content);
     }
 
     async fn reaction_add(&self, _ctx: Context, _add_reaction: Reaction) {
         if let Some(user_id) = _add_reaction.user_id {
-            println!("{} set {} in {}", user_id, _add_reaction.emoji, _add_reaction.message_id)
+            info!("{} set {} in {}", user_id, _add_reaction.emoji, _add_reaction.message_id)
         }
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
-        println!("Successfully started {}", ready.user.name);
+        info!("Successfully started reaction role bot on client '{}#{}'", ready.user.name, ready.user.discriminator);
     }
 }
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
+    debug!("Fetching bot token from environment");
     let token = env::var("BOT_TOKEN").expect("Expected a discord bot token in the environment.");
+    debug!("Successfully fetched bot token from the environment");
 
-    println!("{}", token);
-
+    debug!("Creating discord client");
     let mut client = Client::builder(token)
         .event_handler(Handler)
         .intents(GatewayIntents::GUILDS | GatewayIntents::GUILD_EMOJIS | GatewayIntents::GUILD_MESSAGES | GatewayIntents::GUILD_MESSAGE_REACTIONS)
         .await
         .expect("An exception occurred while trying to create the discord client.");
+    debug!("Successfully created a discord bot client");
 
     if let Err(error) = client.start().await {
-        println!("Client error: {}", error);
+        error!("Client error: {}", error);
     }
 }
--- bot_dev_start.example.bat
+++ bot_dev_start.example.bat
@@ -2,6 +2,7 @@
 
 cd bot
 
+SET RUST_LOG=debug
 SET BOT_TOKEN=YOURBOTTOKEN
 
 cargo run
